49fdb28d0f39a1d5a52719c1d44205d34a507238
luyang <xieoy@hotmail.com>
2017-08-29 23:09:58 +0800

    Build problem.

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import qualified Data.ByteString as B
 import State
 import StateSample
 
@@ -7,3 +8,6 @@ main :: IO ()
 main = do
     let (_, s) = runState stateSample1 ""
     putStrLn s
+    let ss = B.pack "Hello, world"
+    putStrLn "ddd"
+    
