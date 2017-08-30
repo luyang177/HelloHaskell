25bb4d0b26bc76c169737d9c1402c9953fb3dbbc
luyang <xieoy@hotmail.com>
2017-08-30 23:16:50 +0800

    some

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,9 @@ module Main where
 
 import State
 import StateSample
-import Data.List.Utils
+
 import Data.List
+import Data.List.Utils
 
 printMaybe :: Show a => Maybe a -> IO ()
 printMaybe (Just a) = print a
@@ -13,7 +14,23 @@ process :: String -> String
 process = concat . processLines . lines
 
 processLines :: [String] -> [String]
-processLines aLines = filter (\x -> isInfixOf "FontSize" x) aLines
+processLines = fmap (\x -> 
+    case subIndex "FontSize" x of 
+     Just fontSizePos -> 
+        let currentFontSize = getFontSize x fontSizePos
+            destFontSize = currentFontSize + 3
+        in show destFontSize
+     _ -> x)
+
+getFontSize :: String -> Int -> Int
+getFontSize str fontSizePos = 
+    let numberStart = fontSizePos + length "FontSize=\""
+        restStr = drop numberStart str
+        nextSeperator = elemIndex '\"' restStr
+    in case nextSeperator of 
+        Just nextSeperatorInt -> read $ take nextSeperatorInt restStr 
+        _ -> undefined
+
 --printMaybe $ subIndex "foo" "asdfoobar"
 
 main :: IO ()
