73e6c38dee9580f2ac00357b8210d2e39ed89855
luyang <xieoy@hotmail.com>
2017-08-31 22:36:40 +0800

    some

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,13 @@ import Data.List
 import Data.List.Utils
 
 mkString :: String -> [String] -> String
-mkString sperator = concatMap (++ sperator)
+mkString sperator [] = ""
+mkString sperator [x] = x
+mkString sperator xs = 
+    let sizeExcludeLast = length xs - 1
+        major = take sizeExcludeLast xs
+        last = drop sizeExcludeLast xs
+    in concat $ fmap (++ sperator) major ++ last
 
 subIndex2 :: Eq a => [a] -> Int -> [a] -> Maybe Int
 subIndex2 searchStr 0 srcList = subIndex searchStr srcList
@@ -17,25 +23,24 @@ subIndex2 searchStr startIndex srcList =
 substring :: Int -> Int -> [a] -> [a]
 substring startIndex length = take length . drop startIndex
 
-process :: String -> String
-process = mkString "\n" . processLines . lines
+process :: Int -> String -> String
+process offset = mkString "\n" . processLines offset . lines
 
-processLines :: [String] -> [String]
-processLines = fmap (\x -> 
+processLines :: Int -> [String] -> [String]
+processLines offset = fmap (\x -> 
     case subIndex "FontSize" x of 
-     Just fontSizePos -> getFontSize x fontSizePos 3
+     Just fontSizePos -> makeNewFontLine x (fontSizePos + length "FontSize=\"") offset
      _ -> x)
 
   
-getFontSize :: String -> Int -> Int -> String
-getFontSize str fontSizePos offset = 
-    let numberStart = fontSizePos + length "FontSize=\""        
-        nextSeperator = subIndex2 "\"" numberStart str
+makeNewFontLine :: String -> Int -> Int -> String
+makeNewFontLine str fontSizePos offset = 
+    let nextSeperator = subIndex2 "\"" fontSizePos str
     in case nextSeperator of 
         Just nextSeperatorValue -> 
-            let value = read $ substring numberStart (nextSeperatorValue - numberStart) str
-            in take numberStart str ++ 
-               show (value + offset) ++
+            let fontSize = read $ substring fontSizePos (nextSeperatorValue - fontSizePos) str
+            in take fontSizePos str ++ 
+               show (fontSize + offset) ++
                drop nextSeperatorValue str
         _ -> str
 
@@ -43,4 +48,4 @@ getFontSize str fontSizePos offset =
 main :: IO ()
 main = do
     text <- readFile "E:\\Civ6_FontStyles_zh_Hans_CN.xml"
-    writeFile "E:\\Civ6.xml" (process text)
+    writeFile "E:\\Civ6.xml" (process 3 text)
